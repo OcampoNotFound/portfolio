c71544863582ea4cab5dc1314493e796a5aee01a
Brayan <mrx.source.mrx@gmail.com>
2026-01-27 12:34:47 -0500

    feat: new modal style and remove company image from card

--- src/components/ui/ExCard.tsx
+++ src/components/ui/ExCard.tsx
@@ -8,56 +8,52 @@ export const ExCard = () => {
 
   return (
     <>
-      <div className="experienceContainer">
-        {experience.map((exp) => (
-          <div key={exp.id} className="experienceItem">
-            <button
-              className="modalButton"
-              onClick={() => setSelectedExperience(exp)}
-              aria-label="Open experience details"
+      {experience.map((exp) => (
+        <div key={exp.id} className="experienceItem">
+          <button
+            className="modalButton"
+            onClick={() => setSelectedExperience(exp)}
+            aria-label="Open experience details"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
             >
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path d="M12 5v14M5 12h14" />
-              </svg>
-            </button>
-            <div className="companyHeader">
-              {exp.image && (
-                <img
-                  src={exp.image}
-                  alt={exp.company}
-                  className="companyLogo"
-                />
-              )}
-              <h2 className="companyTitle">{exp.company}</h2>
-            </div>
-            <p className="description">{exp.description}</p>
-            <ul>
-              {exp.tech.map((techs, index) => (
-                <li key={index}>
-                  {Array.isArray(techs) ? (
-                    <>
-                      <span
-                        className="techSvg"
-                        dangerouslySetInnerHTML={{ __html: techs[1] }}
-                      />
-                      <span>{techs[0]}</span>
-                    </>
-                  ) : (
-                    techs
-                  )}
-                </li>
-              ))}
-            </ul>
+              <path d="M15 3h6v6"></path>
+              <path d="M10 14 21 3"></path>
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+            </svg>
+          </button>
+          <div className="companyHeader">
+            <h2 className="companyTitle">{exp.company}</h2>
           </div>
-        ))}
-      </div>
+          <p className="description">{exp.description}</p>
+          <ul>
+            {exp.tech.map((techs, index) => (
+              <li key={index}>
+                {Array.isArray(techs) ? (
+                  <>
+                    <span
+                      className="techSvg"
+                      dangerouslySetInnerHTML={{ __html: techs[1] }}
+                    />
+                    <span>{techs[0]}</span>
+                  </>
+                ) : (
+                  techs
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
 
       <ExModal
         experience={selectedExperience}
